fe95afb062f6a397f8ca3b46c9b82941e63dbde0
hoainamnguyen94 <56288728+hoainamnguyen94@users.noreply.github.com>
2021-09-26 19:49:25 -0500

    LFSPRO 2.0.3 Update

--- R/combined.risk.mpc.R
+++ R/combined.risk.mpc.R
@@ -1,14 +1,14 @@
 combined.risk.mpc <- function(posterior, risk, counselee.id){
   # risk in 5 years
-  risk.5 <- apply(as.matrix(cbind(risk[4], risk[5], risk[5])), 2, as.numeric)
+  risk.5 <- apply(as.matrix(cbind(risk[6], risk[9], risk[9])), 2, as.numeric)
   overall.5 <- risk.5 %*% t(posterior)
   
   # risk in 10 years
-  risk.10 <- apply(as.matrix(cbind(risk[6], risk[7], risk[7])), 2, as.numeric)
+  risk.10 <- apply(as.matrix(cbind(risk[7], risk[10], risk[10])), 2, as.numeric)
   overall.10 <- risk.10 %*% t(posterior)
   
   # risk in 15 years
-  risk.15 <- apply(as.matrix(cbind(risk[8], risk[9], risk[9])), 2, as.numeric)
+  risk.15 <- apply(as.matrix(cbind(risk[8], risk[11], risk[11])), 2, as.numeric)
   overall.15 <- risk.15 %*% t(posterior)
   
   risk <- data.frame(counselee.id, diag(overall.5), diag(overall.10), diag(overall.15))
