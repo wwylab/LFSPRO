facf3e59c2e7690269eb12428f05455b2958221f
Nam H Nguyen <56288728+hoainamnguyen94@users.noreply.github.com>
2023-01-25 15:52:35 -0600

    Allow lfspro.cs to use known genetic testing results

--- R/calLK.cs.R
+++ R/calLK.cs.R
@@ -1,4 +1,4 @@
-calLK.cs <- function (data1, data2, penetrance.all) {
+calLK.cs <- function (data1, data2, penetrance.all, mut.info) {
   invasive.cut <- 99
   id <- data1$id
   num.individual <- length(id)
@@ -67,12 +67,14 @@ calLK.cs <- function (data1, data2, penetrance.all) {
         }
       }
     }
-    mut <- data2.i$test[1]
-    if (!is.na(mut)) {
-      if (mut == 0) {
-        lik[i,2:3] <- 0 
-      } else if (mut == 1) {
-        lik[i,1] <- 0
+    if (mut.info == TRUE) {
+      mut <- data2.i$test[1]
+      if (!is.na(mut)) {
+        if (mut == 0) {
+          lik[i,2:3] <- 0 
+        } else if (mut == 1) {
+          lik[i,1] <- 0
+        }
       }
     }
   }
