4120206514e4bb92343a8da9df27342784b69fa3
Nam H Nguyen <56288728+hoainamnguyen94@users.noreply.github.com>
2023-01-25 15:30:42 -0600

    Change the function interface

--- R/calLK.cs.R
+++ R/calLK.cs.R
@@ -1,83 +1,67 @@
-calLK.cs <-
-function (fam.cancer.data, penetrance.all) 
-{
+calLK.cs <- function (data1, data2, penetrance.all) {
   invasive.cut <- 99
-  num.individual <- length(fam.cancer.data$id)
+  id <- data1$id
+  num.individual <- length(id)
   lik <- matrix(NA, nrow = num.individual, ncol = 3)
   length.pene <- nrow(penetrance.all$fMX)
   for (i in 1:num.individual) {
-    if (fam.cancer.data$num.cancer[i] == 0) # no.cancer
-    {
-      if (is.na(fam.cancer.data$gender[i])) # gender mssing
-      {
+    data2.i <- data2[data2$id == id[i],]
+    num.cancer <- nrow(data2.i)-1
+    gender <- data1$gender[i]
+    age <- data2.i$time[num.cancer+1]
+    if (num.cancer == 0) {
+      if (is.na(gender)) {
         pene.mix <- (penetrance.all$fMX + penetrance.all$fFX)/2
-        lik[i, ] <- lkNoneAffect.cs(pene.mix, fam.cancer.data$age[i])
-      }
-      else { # gender not missing
-        if (fam.cancer.data$gender[i] == 1) # male
-        {
-          lik[i, ] <- lkNoneAffect.cs(penetrance.all$fMX, 
-                                      fam.cancer.data$age[i])
-        }
-        else { # female
-          lik[i, ] <- lkNoneAffect.cs(penetrance.all$fFX, 
-                                      fam.cancer.data$age[i])
+        lik[i, ] <- lkNoneAffect.cs(pene.mix, age)
+      } else {
+        if (gender == 1) {
+          lik[i, ] <- lkNoneAffect.cs(penetrance.all$fMX, age)
+        } else {
+          lik[i, ] <- lkNoneAffect.cs(penetrance.all$fFX, age)
         }
       }
-    }
-    else { # if cancer diagnosed
+    } else { 
       flag.benign <- TRUE
-      for (j in 1:fam.cancer.data$num.cancer[i]) {
-        if (fam.cancer.data$cancer.info[[i]]$cancer.type[j] < invasive.cut) {
+      for (j in 1:num.cancer) {
+        cancer.type <- lfspro.cancer.type[data2.i$cancer.type[j]]
+        if (cancer.type < invasive.cut) {
           flag.benign <- FALSE
           break
         }
       }
-      if (flag.benign) 
-      { # if benign
-        if (is.na(fam.cancer.data$gender[i])) { # gender missing
+      if (flag.benign) {
+        if (is.na(gender)) { 
           pene.mix <- (penetrance.all$fMX + penetrance.all$fFX)/2
-          lik[i, ] <- lkNoneAffect.cs(pene.mix, fam.cancer.data$age[i])
-        }
-        else { # gender not missing
-          if (fam.cancer.data$gender[i] == 1) { # male
-            lik[i, ] <- lkNoneAffect.cs(penetrance.all$fMX, 
-                                        fam.cancer.data$age[i])
-          }
-          else { # female
-            lik[i, ] <- lkNoneAffect.cs(penetrance.all$fFX, 
-                                        fam.cancer.data$age[i])
+          lik[i, ] <- lkNoneAffect.cs(pene.mix, age)
+        } else { 
+          if (gender == 1) {
+            lik[i, ] <- lkNoneAffect.cs(penetrance.all$fMX, age)
+          } else {
+            lik[i, ] <- lkNoneAffect.cs(penetrance.all$fFX, age)
           }
         }
-      }
-      else { # not benign
-        # age at-diagnosed
-        temp.id <- which.min(fam.cancer.data$cancer.info[[i]]$diag.age)
-        age.tmp <- fam.cancer.data$cancer.info[[i]]$diag.age[temp.id]
+      } else {
+        temp.id <- which.min(data2.i$time[1:num.cancer])
+        age.tmp <- data2.i$time[temp.id]
         if (age.tmp < 1) {
-          age.tmp = 1
+          age.tmp <- 1
         }
         if (age.tmp > length.pene) {
-          age.tmp = length.pene
+          age.tmp <- length.pene
         }
-        temp.diag.cancer <- fam.cancer.data$cancer.info[[i]]$cancer.type[temp.id]
-        
+        temp.diag.cancer <- lfspro.cancer.type[data2.i$cancer.type[temp.id]]
         {
           d <- if (temp.diag.cancer == 4) 1
           else if (temp.diag.cancer == 1) 2 
           else if (temp.diag.cancer == 2) 2
           else 3
         }
-        
-        if (is.na(fam.cancer.data$gender[i])) 
-        { # gender.missing
+        if (is.na(gender)) { 
           lik[i, ] <- (penetrance.all$fMX[age.tmp, , d] + penetrance.all$fFX[age.tmp, , d])/2
-        }
-        else { # gender.not.missing
-          if (fam.cancer.data$gender[i] == 1) { # male
+        } else { 
+          if (gender == 1) {
             lik[i, ] <- penetrance.all$fMX[age.tmp, , d]
-          }
-          else { # female 
+          } else {
             lik[i, ] <- penetrance.all$fFX[age.tmp, , d]
           }
         }
