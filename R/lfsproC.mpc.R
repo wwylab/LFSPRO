029d443fb54e11e2711aa37bc4945ae768a9a6e6
Nam H Nguyen <56288728+hoainamnguyen94@users.noreply.github.com>
2023-01-25 15:36:30 -0600

    Change the function interface

--- R/lfsproC.mpc.R
+++ R/lfsproC.mpc.R
@@ -1,8 +1,8 @@
 lfsproC.mpc <- function (data1, data2, counselee.id, parameter, allef, nloci, mRate) {
   lik <- calLK.mpc(data1, data2, parameter)
-  id <- as.integer(data1$ID)
-  fid <- as.integer(data1$FatherID)
-  mid <- as.integer(data1$MotherID)
+  id <- as.integer(data1$id)
+  fid <- as.integer(data1$fid)
+  mid <- as.integer(data1$mid)
   counselee.id <- as.integer(counselee.id)
   if (min(id) == 0) {
     id <- id + 1
@@ -12,7 +12,7 @@ lfsproC.mpc <- function (data1, data2, counselee.id, parameter, allef, nloci, mR
   }
   fid[is.na(fid)] <- 0
   mid[is.na(mid)] <- 0
-  ped <- data.frame(ID = id, Gender = 2-data1$Gender, 
+  ped <- data.frame(ID = id, Gender = 2-data1$gender, 
                     FatherID = fid, MotherID = mid, stringsAsFactors = FALSE)
   #browser()
   pp <- peelingRC(allef, lik, ped, counselee.id, nloci, mRate)
