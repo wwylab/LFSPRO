29c74742dfd63f55c2b5a6838c4792d8262f6f0c
Nam H Nguyen <56288728+hoainamnguyen94@users.noreply.github.com>
2023-01-26 16:34:31 -0600

    Update lfspro.cs to make it compatible with updated lfsproC.cs

--- R/lfspro.cs.R
+++ R/lfspro.cs.R
@@ -1,17 +1,10 @@
-.lfspro.cs <-
-function (fam.data, cancer.data, penetrance.all, counselee.id, 
-          allef, nloci, mRate) 
-{
-      
-  # add cancer type for death as 0
-  
+.lfspro.cs <- function (fam.data, cancer.data, counselee.id, 
+                        penetrance.all, allef, nloci, mRate, mut.info = TRUE) {
   num.cancer <- nrow(cancer.data) # number of cancers observed
-  cancer.type.num <- rep(-1, num.cancer)
   colnames(counselee.id) <- c("fam.id", "id")
-  # cancer.type.code -> number
+  
   for (i in 1:num.cancer) {
     tmp <- lfspro.cancer.type[cancer.data$cancer.type[i]]
-    
     if (is.na(tmp)) {
       print(paste("Cannot find cancer ", cancer.data$cancer.type[i], 
                   " in the LFSpro predefined cancer type", sep = ""))
@@ -24,14 +17,17 @@ function (fam.data, cancer.data, penetrance.all, counselee.id,
       colnames(rlt) <- c("fam.id", "id", "pp")
       return(rlt)
     }
-    cancer.type.num[i] <- tmp
   }
-  cancer.data$cancer.type <- cancer.type.num
-  fam.cancer.data <- combinedata(fam.data, cancer.data) # okay!
+  
+  fam.cancer.data <- combinedata(fam.data, cancer.data) 
+  data.obj <- convert.data(fam.cancer.data)
+  data.obj1 <- data.obj[[1]]
+  data.obj2 <- data.obj[[2]]
   
   num.fam <- length(fam.cancer.data)
   pp.all <- NULL
   risk.cs <- NULL
+  
   for (i in 1:num.fam) {
     cid <- counselee.id$id[counselee.id$fam.id == fam.cancer.data[[i]]$fam.id[1]]
     if (length(cid) < 1) {
@@ -39,17 +35,18 @@ function (fam.data, cancer.data, penetrance.all, counselee.id,
                   fam.cancer.data[[i]]$fam.id[1], sep = ""))
       next
     }
-    pp <- lfsproC.cs(fam.cancer.data[[i]], penetrance.all, 
-                     cid, allef, nloci, mRate)
+    pp <- lfsproC.cs(data.obj1[[i]], data.obj2[[i]], cid, 
+                     penetrance.all, allef, nloci, mRate, mut.info)
     pp.tmp <- 1 - pp[,1]
     pp.all <- c(pp.all, pp.tmp)
-    risk.temp <- risk.cs(fam.cancer.data[[i]], lfspenet.cs, cid, pp)
+    risk.temp <- risk.cs(fam.cancer.data[[i]], penetrance.all, cid, pp)
     if (is.null(risk.cs)) {
       risk.cs <- c(risk.cs, risk.temp)
     } else {
       risk.cs <- Map(list,risk.cs,risk.temp)
     }
   }
+  
   pp <- data.frame(cbind(counselee.id, pp.all), check.names = F)
   names(pp)[3] <- "pp"
   output <- list(pp, risk.cs)
