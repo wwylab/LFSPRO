a492a5a643a56dcd1d5cdca033ae86dd0f771423
Nam H Nguyen <56288728+hoainamnguyen94@users.noreply.github.com>
2023-01-26 15:40:03 -0600

    Allow lfspro.cs to use known genetic testing results

--- R/calLK.cs.R
+++ R/calLK.cs.R
@@ -1,14 +1,14 @@
-calLK.cs <- function (data1, data2, penetrance.all, mut.info = TRUE) {
+calLK.cs <- function (ind.data, penetrance.all, mut.info = TRUE) {
   invasive.cut <- 99
-  id <- data1$id
+  id <- unique(ind.data$id)
   num.individual <- length(id)
   lik <- matrix(NA, nrow = num.individual, ncol = 3)
   length.pene <- nrow(penetrance.all$fMX)
   for (i in 1:num.individual) {
-    data2.i <- data2[data2$id == id[i],]
-    num.cancer <- nrow(data2.i)-1
-    gender <- data1$gender[i]
-    age <- data2.i$time[num.cancer+1]
+    ind.data.i <- ind.data[ind.data$id == id[i],]
+    num.cancer <- nrow(ind.data.i)-1
+    gender <- ind.data.i$gender[1]
+    age <- ind.data.i$time[num.cancer+1]
     if (num.cancer == 0) {
       if (is.na(gender)) {
         pene.mix <- (penetrance.all$fMX + penetrance.all$fFX)/2
@@ -23,7 +23,7 @@ calLK.cs <- function (data1, data2, penetrance.all, mut.info = TRUE) {
     } else { 
       flag.benign <- TRUE
       for (j in 1:num.cancer) {
-        cancer.type <- lfspro.cancer.type[data2.i$cancer.type[j]]
+        cancer.type <- lfspro.cancer.type[ind.data.i$cancer.type[j]]
         if (cancer.type < invasive.cut) {
           flag.benign <- FALSE
           break
@@ -41,15 +41,15 @@ calLK.cs <- function (data1, data2, penetrance.all, mut.info = TRUE) {
           }
         }
       } else {
-        temp.id <- which.min(data2.i$time[1:num.cancer])
-        age.tmp <- data2.i$time[temp.id]
+        temp.id <- which.min(ind.data.i$time[1:num.cancer])
+        age.tmp <- ind.data.i$time[temp.id]
         if (age.tmp < 1) {
           age.tmp <- 1
         }
         if (age.tmp > length.pene) {
           age.tmp <- length.pene
         }
-        temp.diag.cancer <- lfspro.cancer.type[data2.i$cancer.type[temp.id]]
+        temp.diag.cancer <- lfspro.cancer.type[ind.data.i$cancer.type[temp.id]]
         {
           d <- if (temp.diag.cancer == 4) 1
           else if (temp.diag.cancer == 1) 2 
@@ -68,7 +68,7 @@ calLK.cs <- function (data1, data2, penetrance.all, mut.info = TRUE) {
       }
     }
     if (mut.info == TRUE) {
-      mut <- data2.i$test[1]
+      mut <- ind.data.i$test[1]
       if (!is.na(mut)) {
         if (mut == 0) {
           lik[i,2:3] <- 0 
